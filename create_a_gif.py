e868d956c3a2cd0510c4c5a5964fbdaabc56c87b
ZankInRN <pedro.ronaldo2786@gmail.com>
2025-10-16 17:31:21 -0300

    Update do Projeto Final - Codedex
    ##  Create a GIF
    
    O projeto tem como objetivo gerar um GIF utilizando fotos escolhidas pelo usuário.

---
+++ create_a_gif.py
@@ -0,0 +1,10 @@
+import imageio.v3 as iio
+
+filenames = ['fotoum.jpg', 'fotodois.jpg']
+images = []
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
